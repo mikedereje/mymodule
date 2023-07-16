3edffaf008e074938697dded5d5aac779f2e01ac
mikedereje <124740677+mikedereje@users.noreply.github.com>
2023-07-16 03:49:34 -0700

    Add files via upload

---
+++ s.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Jhon";
+names[1]="Jason";
+names[2]="Jcole";
+names[3]="monica";
+names[4]="phoebe";
+names[5]="Joey";
+names[6]="rachel";
+names[7]="Jane";
+names[8]="mike";
+
+
+for (var i =0; i<names.length; i++){
+    if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='J'){
+        console.log("goodbye" +  names[i])
+        
+    }
+    else{
+        console.log("hello"+  names[i])
+    }
+}
